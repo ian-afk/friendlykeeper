6c84028a61b833b401919a2f5a3c2d3815edab67
ian-afk <68414797+ian-afk@users.noreply.github.com>
2025-03-12 18:04:03 +0800

    initial structure files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <h1>Friendly Keeper</h1>
+      </div>
+      <div>
+        <ul>
+          <li>Filters</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
